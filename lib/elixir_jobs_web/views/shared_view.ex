397fb97964201692652eb6476540e6f995f0275c
Petr Stepchenko <nitrinodev@gmail.com>
2019-01-10 03:16:49 +0300

    Add show new offer button condition

--- lib/elixir_jobs_web/views/shared_view.ex
+++ lib/elixir_jobs_web/views/shared_view.ex
@@ -1,3 +1,16 @@
 defmodule ElixirJobsWeb.SharedView do
   use ElixirJobsWeb, :view
+
+  alias ElixirJobsWeb.OfferController
+  alias ElixirJobsWeb.PageController
+
+  import Phoenix.Controller, only: [controller_module: 1, action_name: 1]
+
+  def show_new_offer_button(conn) do
+    case {controller_module(conn), action_name(conn)} do
+      {OfferController, :index} -> true
+      {PageController, :about} -> true
+      _ -> false
+    end
+  end
 end
